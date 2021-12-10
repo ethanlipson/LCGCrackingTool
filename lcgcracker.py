3305f0b0dc16f19367bd2503e950c07164cab55c
Ian Graham Martinez <Ian.graham28@gmail.com>
2021-12-10 11:20:48 -0500

    optimized

--- lcgcracker.py
+++ lcgcracker.py
@@ -1,30 +1,52 @@
 #feel free to rename this file, lcgcracker sounds lame :(
 
-def primeFactors(n):
-	primes = []
+def primes(n):
+	out = []
 	while n % 2 == 0:
 		n /= 2
-		primes.append(2)
-	factor = 3
-	while n > 1:
-		if n % factor == 0:
-			primes.append(factor)
-			n /= factor
+		out.append(2)
+	f = 3
+	while f * f <= n:
+		if n % f == 0:
+			out.append(f)
+			n /= f
 		else:
-			factor += 2
-	return primes
+			f += 2
+	if n != 1:
+		out.append(n)
+	return out
 
 #cool trick I came up with to calculate all factors quickly.
 #doing it the brute force way took like 5 seconds for big numbers.
 #numbers are not in order.
 def factors(n):
-	primes = primeFactors(n)
-	count = len(primes)
+	prime = primes(n)
+	count = len(prime)
 	out = set()
 	for i in range(0,2**count):
 		factor = 1
 		for j in range(0, count):
-			factor *= primes[j] if i%2 else 1
+			factor *= prime[j] if i%2 else 1
 			i /= 2
 		out.add(factor)
 	return out
+
+def lcm(a,b):
+	a_p=primes(a)
+	b_p=primes(b)
+	print(b_p)
+	out = 1
+	while len(a_p) > 0 or len(b_p) > 0:
+		a_l = a_p[-1] if len(a_p) else 1
+		b_l = b_p[-1] if len(b_p) else 1
+
+		if a_l >= b_l:
+			a_p.pop()
+		if a_l <= b_l:
+			b_p.pop()
+
+		out *= max(a_l,b_l)
+
+	return out
+
+print(lcm(36392598, 5941123987))
